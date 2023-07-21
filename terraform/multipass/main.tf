e760615395a623f5752a46c6c1008ded6306d225
Gauthier Leonard <gauthier@e36knots.com>
2023-07-21 11:24:22 +0200

    feat: add terraform + multipass conf

---
+++ terraform/multipass/main.tf
@@ -0,0 +1,66 @@
+# Provider: multipass
+terraform {
+  required_providers {
+    multipass = {
+      source  = "larstobi/multipass"
+      version = "1.4.2"
+    }
+  }
+}
+
+provider "multipass" {}
+
+# Multipass instances
+resource "multipass_instance" "validators" {
+  for_each = {
+    for i in range(1, 6) : "validator0${i}" => i
+  }
+
+  name           = each.key
+  image          = "jammy"
+  cpus           = 1
+  memory         = "1GiB"
+  disk           = "20GiB"
+  cloudinit_file = "../../files/multipass/cloud-init.yaml"
+}
+
+data "multipass_instance" "validators_info" {
+  depends_on = [
+    multipass_instance.validators
+  ]
+
+  for_each = {
+    for i in range(1, 6) : "validator0${i}" => i
+  }
+
+  name = each.key
+}
+
+resource "multipass_instance" "frontend" {
+  name           = "frontend"
+  image          = "jammy"
+  cpus           = 2
+  memory         = "2GiB"
+  disk           = "20GiB"
+  cloudinit_file = "../../files/multipass/cloud-init.yaml"
+}
+
+data "multipass_instance" "frontend_info" {
+  depends_on = [
+    multipass_instance.frontend
+  ]
+
+  name = "frontend"
+}
+
+# Ansible hosts templating
+resource "local_file" "ansible_hosts" {
+  content = templatefile(
+    "hosts.tftpl",
+    {
+      validators = values(data.multipass_instance.validators_info)
+      frontend   = data.multipass_instance.frontend_info
+    }
+  )
+  filename = "hosts.ini"
+}
