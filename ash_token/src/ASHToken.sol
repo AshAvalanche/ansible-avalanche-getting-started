20d6ae9b186c0b4143e9770337f4719c9c1296f6
gaetan cabaret <gaetan@e36knots.com>
2024-02-13 16:12:33 +0100

    fix(ASHToken): Change token name

--- ash_token/src/ASHToken.sol
+++ ash_token/src/ASHToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ASHToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Ash", "ASH") {
+    constructor(uint256 initialSupply) ERC20("ASHToken", "AT") {
         _mint(msg.sender, initialSupply);
     }
 }
