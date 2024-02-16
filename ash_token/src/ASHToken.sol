dfa2aa1f8c04408fd734c55e863ddbd39489a35f
Gaétan Cabaret <57412868+servalD@users.noreply.github.com>
2024-02-16 14:26:47 +0100

    feat: add a sol files for foundry (#41)

---
+++ ash_token/src/ASHToken.sol
@@ -0,0 +1,11 @@
+// contracts/ASHToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ASHToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("ASHToken", "AT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
